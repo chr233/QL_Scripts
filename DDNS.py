91c8afa70ae6572b5901c57d7ba223fe347b2e38
Chr_ <chr@chrxw.com>
2022-02-06 17:55:04 +0800

    修改日志输出

--- DDNS.py
+++ DDNS.py
@@ -2,16 +2,20 @@
 # @Author       : Chr_
 # @Date         : 2022-02-06 16:59:08
 # @LastEditors  : Chr_
-# @LastEditTime : 2022-02-06 17:43:16
+# @LastEditTime : 2022-02-06 17:54:48
 # @Description  : 启动文件
 '''
 
 from cddns import ddns
 from os import path
 
+import logging
+
+logging.getLogger("requests").setLevel(logging.WARNING)
+
 SCRIPT_PATH = path.split(path.realpath(__file__))[0]
 CONFIG_PATH = path.join(SCRIPT_PATH, 'ddns.toml')
-CACHE_PATH = path.join('/tmp/ip.txt')
+CACHE_PATH = '/tmp/ip.txt'
 
 
 ddns(CONFIG_PATH, CACHE_PATH)
