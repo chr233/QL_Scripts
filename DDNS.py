d3ebfb59046b9845fecc657390b92e96f0c6c221
Chr_ <chr@chrxw.com>
2022-02-06 17:43:31 +0800

    更新ip.txt位置

--- DDNS.py
+++ DDNS.py
@@ -2,7 +2,7 @@
 # @Author       : Chr_
 # @Date         : 2022-02-06 16:59:08
 # @LastEditors  : Chr_
-# @LastEditTime : 2022-02-06 17:24:55
+# @LastEditTime : 2022-02-06 17:43:16
 # @Description  : 启动文件
 '''
 
@@ -11,7 +11,7 @@ from os import path
 
 SCRIPT_PATH = path.split(path.realpath(__file__))[0]
 CONFIG_PATH = path.join(SCRIPT_PATH, 'ddns.toml')
-CACHE_PATH = path.join(SCRIPT_PATH, 'ip.txt')
+CACHE_PATH = path.join('/tmp/ip.txt')
 
 
 ddns(CONFIG_PATH, CACHE_PATH)
